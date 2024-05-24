46e0309ecef80d444e4a1abb790bc13e38035671
my-gitadv <164844995+my-gitadv@users.noreply.github.com>
2024-05-24 16:20:13 +0700

    Add files via upload

---
+++ DATA-STRUCK-Adventure/Demo3LinkedListGPA/Demo3LinkedListGPA/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Demo3LinkedListGPA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Array db = new Array(10);
+            db.addStudent(new Student("Matthew", "5906001", 3.5));
+            db.addStudent(new Student("Mark", "5906002", 2.75));
+            Student top = db.findTopStudent();
+            Console.WriteLine("The top student is " + top.name + "id = " + top.student_id);
+        }
+    }
+}
---
+++ DATA-STRUCK-Adventure/Demo3LinkedListGPA/Demo3LinkedListGPA/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3LinkedListGPA
+{
+    internal class Student
+    {
+        public String name;
+        public String student_id;
+        public double gpa;
+
+        public Student(String name, String student_id, int gpa)
+        {
+            this.name = name;
+            this.student_id = student_id;
+            this.gpa = gpa;
+        }
+    }
+
+    
+}
