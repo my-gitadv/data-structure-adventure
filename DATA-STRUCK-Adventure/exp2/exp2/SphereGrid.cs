3016a5b3302bca69c2f20a785106888593d5198e
my-gitadv <164844995+my-gitadv@users.noreply.github.com>
2024-05-24 16:23:28 +0700

    Add files via upload

---
+++ DATA-STRUCK-Adventure/exp2/exp2/SphereGrid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exp2
+{
+
+    public class SphereGrid
+
+    {
+        Sphere[,] arr;
+        int size;
+        int cap;
+
+        public SphereGrid(int cap) {
+
+            arr = new Sphere[cap,cap];
+            size = 0;
+            this.cap = cap;
+        }
+
+        public void insertSphere(Sphere s, int i, int j)
+        {
+            arr[i,j] = s;
+        }
+        public void run()
+        {
+            Sphere ss = arr[0,0];
+            while(ss.nextI != -1)
+            {
+                Console.WriteLine(ss.name + " +" + ss.stat);
+                ss = arr[ss.nextI, ss.nextJ];
+            }
+            Console.WriteLine(ss.name + " +" + ss.stat);
+
+        }
+
+
+    }
+}
