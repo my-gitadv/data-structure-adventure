cee9d2a6244e2fff9c115c131094499dc20a7d02
my-gitadv <164844995+my-gitadv@users.noreply.github.com>
2024-07-22 16:42:47 +0700

    Add files via upload

---
+++ lab1/lab1/DynamicArray.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+public class DynamicArray
+{
+    private int[] arr;
+    private int capacity;
+    private int size; // Last element can be indexed at size-1
+
+    public DynamicArray(int cap)
+    { // Class Constructor
+        arr = new int[cap];
+        capacity = cap;
+        size = 0;
+    }
+
+    public void pushBack(int data)
+    {
+        if (size == capacity)
+        {
+            int[] temp = new int[2 * capacity];
+
+            for (int i = 0; i < size; i++) 
+            {
+                temp[i] = arr[i];
+            }
+            arr = temp;
+            capacity *= 2;
+        }
+        arr[size] = data;
+        size++;
+    }
+
+    public int popBack()
+    {
+        if (size == 0)
+        {
+            Console.WriteLine("ERROR");
+            return arr[0];
+        }
+        else
+        {
+            size--;
+            return arr[size];
+        }
+
+    }
+
+    public int get(int i)
+    { 
+        if (i>=size)
+        {
+           Console.WriteLine("ERROR");   
+        }
+        return arr[i];
+        
+        
+    }
+    // [1,3,4,6,7,0,0]
+    // i = 3
+    public void set(int i, int value)
+    {
+        if (i>=size) 
+        {
+            Console.WriteLine("ERROR");
+        }
+        else
+        {
+            arr[i] = value;
+        }
+        
+    }
+
+    public void remove(int i)
+    {
+        if (i >= size)
+        {
+            Console.WriteLine("ERROR");
+        }
+        else
+        {
+            for (int j = i; j < size; j++)
+            {
+                arr[j] = arr[j + 1];
+            }
+            size--;
+        }
+
+    }
+
+    public bool isEmpty()
+    {
+        if (size != 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+        
+    }
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public void printStructure()
+    {
+
+        Console.Write("Size = " + size + ", Cap = " + capacity + ", array = ");
+
+        Console.Write("[ ");
+
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write(arr[i] + ", ");
+        }
+        Console.WriteLine("]");
+    }
+}
---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+        Dynamicarray var = new dynamicarray(2);
+        var.printstructure();
+        var.pushback(5);
+        var.pushback(6);
+        var.printstructure();
+        var.pushback(7);
+        var.printstructure();
+        var.pushback(8);
+        var.printstructure();
+        var.pushback(9);
+        var.printstructure();
+
+
+
+
+        // DynamicArray var = new DynamicArray(2);
+        // for (int i = 0; i < 10; i++)
+        // {
+        //     var.pushBack(i * i + 1);
+        // }
+        // var.printStructure();
+        // Console.WriteLine(var.get(3));
+        // var.set(3, 555);
+        // Console.WriteLine(var.get(3));
+        // var.remove(3);
+        // var.printStructure();
+
+
+
+
+
+        // DynamicArray var = new DynamicArray(2);
+        // for (int i = 0; i < 10; i++)
+        // {
+        //     var.pushBack(i * i + 1);
+        // }
+        // var.printStructure();
+        // Console.WriteLine(var.get(10));
+        // var.set(10, 555);
+        // var.remove(10);
+        // var.printStructure();
+
+
+
+
+        // DynamicArray var = new DynamicArray(2);
+        // Console.WriteLine(var.popBack());
+        // var.pushBack(555);
+        // Console.WriteLine(var.popBack());
+
+
+
+
+        // DynamicArray var = new DynamicArray(2);
+        // Console.WriteLine(var.isEmpty());
+        // var.pushBack(555);
+        // Console.WriteLine(var.isEmpty());
+
+
+
+        // DynamicArray var = new DynamicArray(2);
+        // Console.WriteLine(var.getSize());
+        // var.pushBack(555);
+        // Console.WriteLine(var.getSize());
+
+    }
+}
